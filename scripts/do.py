3e4b1f27ce668dd975b33321a77224512f45f4fd
hdknr <gmail@hdknr.com>
2014-02-05 05:15:56 +0900

    do.py; managements

--- scripts/do.py
+++ scripts/do.py
@@ -1,14 +1,24 @@
+#!/usr/bin/env python
+# django version 1.5
 import sys
 import re
-import parser
+import os
 
-pat = r"^\s*execute_from_command_line"
-manage_py = "".join([i for i in open(sys.argv[1]).readlines()
-                     if not re.search(pat, i)])
+pat = r'(?P<indent>\s+)(?P<code>execute_from_command_line.+)$'
+patch = '''from django.conf import settings
+settings.INSTALLED_APPS = settings.INSTALLED_APPS + ('djado',)'''
 
-eval(parser.suite(manage_py).compile())
-
-del sys.argv[0]
-from django.conf import settings
-settings.INSTALLED_APPS = settings.INSTALLED_APPS + ('djado,')
-execute_from_command_line(sys.argv)
+if len(sys.argv) > 1 and os.path.basename(sys.argv[1]) == 'manage.py':
+    print "Createing a do.py...."
+    dst = os.path.join(os.path.dirname(os.path.abspath(sys.argv[1])),
+                       'do.py')
+    with open(dst, "w") as out, open(sys.argv[1]) as src:
+        for i in src.readlines():
+            m = re.search(pat, i)
+            m = m and m.groupdict()
+            if m:
+                for p in patch.split('\n'):
+                    out.write(m['indent'])
+                    out.write(p)
+                    out.write("\n")
+            out.write(i)
