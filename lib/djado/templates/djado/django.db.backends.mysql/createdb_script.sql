98eea41c46f8169ac18f8ff27c4194c5d30d6c68
hdknr <gmail@hdknr.com>
2016-07-08 08:36:29 +0000

    client host address

--- lib/djado/templates/djado/django.db.backends.mysql/createdb_script.sql
+++ lib/djado/templates/djado/django.db.backends.mysql/createdb_script.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE {{ db.NAME }}
 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 GRANT ALL on {{ db.NAME }}.*
-to '{{ db.USER }}'@'{{ db.HOST|default:'localhost' }}'
-identified by '{{ db.PASSWORD }}' WITH GRANT OPTION;
+to '{{ db.USER }}'@'%' identified by '{{ db.PASSWORD }}' WITH GRANT OPTION;
